f691c7c43ae049fac6257eecc07dd4516db7f91d
Krešimir Prcela <kresimir.prcela@gmail.com>
2025-04-21 09:33:26 +0200

    wip

--- Sources/Avatar/AvatarView.swift
+++ Sources/Avatar/AvatarView.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 
 public class AvatarView: UIImageView {
+    public static var hideAll = false
     public var avatarId: Int64? = nil {
         didSet {
             if oldValue != avatarId {
@@ -18,8 +19,26 @@ public class AvatarView: UIImageView {
         }
     }
     
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        isHidden = Self.hideAll
+    }
+    
+    override public var isHidden: Bool {
+        get {
+            return super.isHidden
+        }
+        set {
+            if Self.hideAll {
+                super.isHidden = true
+            } else {
+                super.isHidden = newValue
+            }
+        }
+    }
+    
     fileprivate func update() {
-        if let avatarId {
+        if let avatarId, !Self.hideAll {
             image = AvatarCache.fetchImage(avatarId: avatarId, small: false)
         }
     }
